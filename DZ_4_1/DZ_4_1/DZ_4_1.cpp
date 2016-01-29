cda61e9310965774250dabf2c16f05166dc24ec7
igor-nplsk <igor-nplsk@mail.ru>
2016-01-29 12:39:41 +0300

    homework 4 ready
    @vrutkovskiy @swordix @marina-im @sprightly1991 @antonzaitsev
    @isadchenko check please

---
+++ DZ_4_1/DZ_4_1/DZ_4_1.cpp
@@ -0,0 +1,163 @@
+﻿//Создать абстрактный базовый класс Figure с виртуальными методами вычисления площади и периметра.
+//Создать производные классы : Rectangle(прямоугольник), Circle(круг), Trapezium(трапеция)
+//со своими функциями площади и периметра.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+class Figure
+{
+public:
+	Figure() = default;
+	Figure(const Figure& fig) = delete;
+	const Figure& operator=(const Figure& fig) = delete;
+	virtual ~Figure()
+	{}
+	virtual double Area() = 0;
+	virtual double Perimeter() = 0;
+};
+
+class Circle : public Figure
+{
+	double r;
+public:
+	Circle() = default;
+	Circle(double Radius) : r(Radius)
+	{}
+	Circle(const Circle& cir) = delete;
+	const Circle& operator=(const Circle& cir) = delete;
+	~Circle()
+	{}
+	void SetData()
+	{
+		cout << "Input radius: ";
+		cin >> r;
+	}
+	double Area() override
+	{
+		return 3.14*r*r;
+	}
+	double Perimeter() override
+	{
+		return 2 * 3.14*r;
+	}
+};
+
+class Rectangle : public Figure
+{
+	double a;
+	double b;
+public:
+	Rectangle() = default;
+	Rectangle(double Side1, double Side2) : a(Side1), b(Side2)
+	{}
+	Rectangle(const Rectangle& rec) = delete;
+	const Rectangle& operator=(const Rectangle& rec) = delete;
+	~Rectangle()
+	{}
+	void SetData()
+	{
+		cout << "Input side1: ";
+		cin >> a;
+		cout << "Input side2: ";
+		cin >> b;
+	}
+	double Area() override
+	{
+		return a*b;
+	}
+	double Perimeter() override
+	{
+		return (a + b) * 2;
+	}
+};
+
+class Trapezium : public Figure
+{
+	double a;
+	double b;
+	double c;
+	double d;
+public:
+	Trapezium() = default;
+	Trapezium(double Side1, double Side2, double Side3, double Side4) : a(Side1), b(Side2), c(Side3), d(Side4)
+	{}
+	Trapezium(const Trapezium& tra) = delete;
+	const Trapezium& operator=(const Trapezium& tra) = delete;
+	void SetData()
+	{
+		cout << "Input bigger parallel side1: ";
+		cin >> a;
+		cout << "Input lesser parallel side2: ";
+		cin >> b;
+		cout << "Input side3: ";
+		cin >> c;
+		cout << "Input side4: ";
+		cin >> d;
+	}
+	double Area() override
+	{
+		double h;
+		h = sqrt(pow(c, 2) - pow(((pow((a - b), 2) + pow(c, 2) - pow(d, 2)) / (2 * (a - b))), 2));
+		return (a + b)*h / 2;
+	}
+	double Perimeter() override
+	{
+		return a + b + c + d;
+	}
+};
+
+int main()
+{
+	int choice;
+
+	do{
+		cout << "Choose figure:" << endl;
+		cout << "1. Rectangle " << endl;
+		cout << "2. Circle " << endl;
+		cout << "3. Trapezium " << endl;
+		cout << "4. Exit" << endl;
+
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+		{
+				  Rectangle R;
+				  R.SetData();
+				  cout << "Area of Rectangle = " << R.Area() << endl;
+				  cout << "Perimeter of Rectangle = " << R.Perimeter() << endl << endl;
+				  break;
+		}
+		case 2:
+		{
+				  Circle C;
+				  C.SetData();
+				  cout << "Area of Circle = " << C.Area() << endl;
+				  cout << "Perimeter of Circle = " << C.Perimeter() << endl << endl;
+				  break;
+		}
+		case 3:
+		{
+				  Trapezium T;
+				  T.SetData();
+				  cout << "Area of Trapezium = " << T.Area() << endl;
+				  cout << "Perimeter of Trapezium = " << T.Perimeter() << endl;
+				  break;
+		}
+		case 4:
+		{
+				  exit(1);
+		}
+		default:
+		{
+				   cout << "Incorrect choice!" << endl;
+				   break;
+		}
+		}
+	} while (true);
+
+	return 0;
+}
---
+++ DZ_4_2/DZ_4_2/DZ_4_2.cpp
@@ -0,0 +1,127 @@
+﻿//Создать класс Triangle для представления треугольника.Поля данных являются сторонами треугольника.
+//Определить подходящие исключения.Должны быть реализованы операции получения и изменения полей данных,
+//вычисления площади, вычисления периметра, вычисления высот, вычисления углов,
+//определения вида треугольника(равносторонний, равнобедренный, прямоугольный).
+//В случае возникновения ошибок методы должны генерировать исключения.
+
+#include <iostream>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+#define PI 3.14159265
+
+class Triangle {
+private:
+	double side1;
+	double side2;
+	double side3;
+	double angle1, angle2, angle3;
+public:
+	Triangle() = default;
+	Triangle(const double& s1, const double& s2, const double& s3) : side1(s1), side2(s2), side3(s3)
+	{}
+	Triangle(const Triangle& obj) = delete;
+	const Triangle& operator=(const Triangle& obj) = delete;
+	~Triangle()
+	{}
+
+	class Negative
+	{};
+
+	void set_sides()
+	{
+		cout << "Input sides of Triangle(side1 side2 side3): " << endl;
+		cin >> side1 >> side2 >> side3;
+
+		if (side1 < 0 || side2 < 0 || side3 < 0)
+			throw Negative();
+	}
+
+	void square()
+	{
+		double area, P;
+		P = (side1 + side2 + side3) / 2;
+		area = sqrt(P*(P - side1)*(P - side2)*(P - side3));
+		cout << "Area of Triangle: " << area << endl;
+	}
+
+	void perimeter()
+	{
+		double perimeter;
+		perimeter = side1 + side2 + side3;
+		cout << "Perimeter of Triangle: " << perimeter << endl;
+	}
+
+	void hight()
+	{
+		double h1, h2, h3, p;
+		p = (side1 + side2 + side3) / 2;
+		h1 = sqrt(4 * p*((p - side1)*(p - side2)*(p - side3)) / (side1*side1));
+		h2 = sqrt(4 * p*((p - side1)*(p - side2)*(p - side3)) / (side2*side2));
+		h3 = sqrt(4 * p*((p - side1)*(p - side2)*(p - side3)) / (side3*side3));
+		cout << "hight1: " << h1 << " " << "hight2: " << h2 << " " << "hight3: " << h3 << endl;
+	}
+
+	void angle()
+	{
+		angle1 = acos((side2*side2 + side3*side3 - side1*side1) / (2 * side2*side3))*180.0 / PI;
+		angle2 = acos((side1*side1 + side3*side3 - side2*side2) / (2 * side1*side3))*180.0 / PI;
+		angle3 = acos((side2*side2 + side1*side1 - side3*side3) / (2 * side2*side1))*180.0 / PI;
+		cout << "angle1: " << angle1 << "\nangle2: " << angle2 << "\nangle3: " << angle3 << endl;
+	}
+
+	void kind_of_triangle()
+	{
+		if (static_cast<int>(side1) == static_cast<int>(side2) == static_cast<int>(side3))
+			cout << "Equilateral triangle" << endl;
+		if ((static_cast<int>(side1) == static_cast<int>(side2)) ||
+			(static_cast<int>(side2) == static_cast<int>(side3)) ||
+			(static_cast<int>(side3) == static_cast<int>(side1)))
+			cout << "Isosceles triangle" << endl;
+		if ((static_cast<int>(angle1) == 90) || (static_cast<int>(angle2) == 90) || (static_cast<int>(angle3) == 90))
+			cout << "Rectangular triangle" << endl;
+	}
+
+	double get_side1()
+	{
+		return side1;
+	}
+	double get_side2()
+	{
+		return side2;
+	}
+	double get_side3()
+	{
+		return side3;
+	}
+};
+
+int main()
+{
+	try
+	{
+		Triangle T;
+		T.set_sides();
+		if (T.get_side1() + T.get_side2() <= T.get_side3() ||
+			T.get_side2() + T.get_side3() <= T.get_side1() ||
+			T.get_side3() + T.get_side1() <= T.get_side2())
+			throw 11;
+
+		T.square();
+		T.perimeter();
+		T.hight();
+		T.angle();
+		T.kind_of_triangle();
+	}
+	catch (int)
+	{
+		cout << "Triangle is not exist!" << endl;
+	}
+	catch (Triangle::Negative)
+	{
+		cout << "Input negative number!" << endl;
+	}
+
+	return 0;
+}
